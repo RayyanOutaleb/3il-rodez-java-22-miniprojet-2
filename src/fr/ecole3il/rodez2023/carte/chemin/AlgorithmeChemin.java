835aa2808b00871ce858dc3944185ce2a377a3cd
Rayyan O <outalebr@3il.fr>
2024-02-28 09:55:52 +0100

    Changement de place & création AlgorithmeChemin

---
+++ src/fr/ecole3il/rodez2023/carte/chemin/AlgorithmeChemin.java
@@ -0,0 +1,4 @@
+package fr.ecole3il.rodez2023.carte.chemin;
+
+public class AlgorithmeChemin {
+}
--- src/fr/ecole3il/rodez2023/carte/chemin/Graphe.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/Graphe.java
@@ -1,4 +1,4 @@
-package fr.ecole3il.rodez2023.carte.elements;
+package fr.ecole3il.rodez2023.carte.chemin;
 
 import java.util.ArrayList;
 import java.util.HashMap;
